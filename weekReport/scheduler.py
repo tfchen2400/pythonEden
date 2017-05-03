ec100438179e412029c141598bfb19554de3187a
chentf <chentf@mchz.com.cn>
2017-05-03 18:10:12 +0800

    添加查找包的sys.path的路劲

--- weekReport/scheduler.py
+++ weekReport/scheduler.py
@@ -3,11 +3,14 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from weekReport import weekReportHzmc
 import time
+import sys
+import os
 
+sys.path.append(os.path.abspath('.'))
 scheduler = BackgroundScheduler()
 print('start to sleep')
 
 scheduler.add_job(weekReportHzmc.do_all, 'cron', day_of_week='mon-sun', hour='*', minute='0-59', second='*/30')
 scheduler.start()
 while True:
-    time.sleep(1*60*60)  # 其他任务是独立的线程执行
+    time.sleep(1 * 60 * 60)  # 其他任务是独立的线程执行
